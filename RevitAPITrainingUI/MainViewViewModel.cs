d48d7af43926dc97161af8241689fe41a8014a35
vladalexgor@ya.ru <vladalexgor@ya.ru>
2022-03-29 22:23:10 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingUI/MainViewViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPITrainingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingUI
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SelectCommand { get; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SelectCommand = new DelegateCommand(OnSelectCommand);
+        }
+
+        public event EventHandler HideRequest;
+        private void RaiseHideRequest()
+        {
+            HideRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler ShowRequest;
+        private void RaiseShowRequest()
+        {
+            ShowRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnSelectCommand()
+        {
+            RaiseHideRequest();
+
+            Element oElement = SelectionUtils.PickObject(_commandData);
+            TaskDialog.Show("Сообщение", $"ID: {oElement.Id}");
+
+            RaiseShowRequest();
+        }
+    }
+}
